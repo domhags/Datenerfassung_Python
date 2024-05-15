e75f29a3c312ec57ea6a2410a307493b97db3b63
Dominik Hagspiel <051047@edu.szf.at>
2024-05-15 10:10:32 +0200

    Funktionen hinzugefügt: 1. Ändern der Daten 2. Filtern der Daten 15.05.2024 - 10:10 Uhr

--- main.py
+++ main.py
@@ -1,147 +1,158 @@
-import re
-import pandas as pd
+import re  # Modul re für reguläre Ausdrücke
 
 bezeichner_daten = ("Vorname", "Nachname", "Straße", "PLZ", "Ort")  # Tulpe da nicht veränderbar
-UNGUELTIGE_EINGABE = "Ungültige Eingabe! Geben Sie folgendes ein! (ja/nein): \n"
+UNGUELTIGE_EINGABE = "Ungültige Eingabe! Geben Sie folgendes ein! (ja/nein):\n"  # Fehlermeldung (Konstante)
 
 
-def eingabe_daten():
-    gespeicherte_daten = pd.DataFrame()  # DataFrame initialisieren
-    # Schleife zur Dateneingabe
-    while True:
-        # Dictionary für jede einzelne Person erstellen
-        personen_daten = {}
-
-        for bezeichner in bezeichner_daten:
-            while True:
-                eingabe = input(f"Bitte geben Sie {bezeichner} ein: ")  # Benutzereingabe erfassen
-                if eingabe.strip() == "":
-                    print("Feld darf nicht leer sein")
-                    continue
-                break
-
-            if not validierung(eingabe, bezeichner_daten.index(bezeichner)):
-                print("Ungültige Eingabe")
-                continue
-
-            # Daten zum Dictionary hinzufügen
-            personen_daten[bezeichner] = eingabe
-
-        # Dictionary zum DataFrame hinzufügen
-        gespeicherte_daten = pd.concat([gespeicherte_daten, pd.DataFrame([personen_daten])], ignore_index=True)
-
-        # Abfrage zur weiteren Personenerfassung
-        weitere_person = input("Möchten Sie weitere Personen erfassen? (ja/nein):\n")
-        if weitere_person == "j":
-            continue
-        elif weitere_person == "n":
-            abfrage(gespeicherte_daten)
-        else:
-            print(UNGUELTIGE_EINGABE)
-
-    gespeicherte_daten.reset_index(drop=True, inplace=True)  # Index zurücksetzen
-    return gespeicherte_daten
-
-
-def ausgabe_daten(gespeicherte_daten):
-    # Schleife zur Filterung
-    while True:
-        auswahl_filtern = input("Möchten Sie die Liste filtern? (ja/nein)):\n")
-        if auswahl_filtern == "j":
-            print("Verfügbare Bezeichner:")
-            for i, bezeichner in enumerate(gespeicherte_daten.columns, 1):  # Index beginnt bei 1
-                print(f"{i}. {bezeichner}")
-
-            bezeichner_nr = int(input("Bitte geben Sie die Nummer des Bezeichners ein, "
-                                      "nach dem Sie filtern möchten: ")) - 1  # -1 da Index normalerweise bei 0 beginnt
-            bezeichner = gespeicherte_daten.columns[bezeichner_nr]
-            filterkriterium = input(f"Bitte geben Sie das Filterkriterium für '{bezeichner}' ein: ")
-
-            gefilterte_liste = gespeicherte_daten.query(f"{bezeichner} == '{filterkriterium}'")
-            print("Gefilterte Daten:")
-            print(gefilterte_liste)
-        elif auswahl_filtern == "n":
-            break
-        else:
-            print(UNGUELTIGE_EINGABE)
-
-        # Abfrage zur Ausgabe der Daten
-    while True:
-        ausgabe_aufforderung = input("Möchten Sie die Daten ausgeben lassen? (ja/nein):\n")
-        if ausgabe_aufforderung == "j":
-            print("Gespeicherte Daten:\n" + gespeicherte_daten.to_string())  # String umwandeln
-            exit()  # Programm wird geschlossen
-        elif ausgabe_aufforderung == "n":
-            print("Das Programm wird beendet!")
-            exit()
-        else:
-            print(UNGUELTIGE_EINGABE)
-
-
+# Funktion zur Validierung der Eingaben
 def validierung(daten, bezeichner_index, ):
     validierungsregeln = {
-        0: r'^[A-Za-zÄÖÜäöüß-]+$',  # Vorname
-        1: r'^[A-Za-zÄÖÜäöüß-]+$',  # Nachname
-        2: r'^[A-Za-zÄÖÜäöüß0-9\s.,/-]+$',  # Straße
-        3: r'^\d{4}$',  # PLZ
-        4: r'^[A-Za-zÄÖÜäöüß\s]+$',  # Ort
+        0: r'^[A-Za-zÄÖÜäöüß-]+$',  # Vorname (r' = roher String)
+        1: r'^[A-Za-zÄÖÜäöüß-]+$',  # Nachname (^ = Anfang des Strings)
+        2: r'^[A-Za-zÄÖÜäöüß0-9\s.,/-]+$',  # Straße ([] = Zeichen die erlaubt sind und \s = Leerzeichen)
+        3: r'^\d{4}$',  # PLZ (\d = 4 Ziffern folgend für Österreich)
+        4: r'^[A-Za-zÄÖÜäöüß\s]+$',  # Ort (+ = Mindestens einmal vorkommen und $ = Ende vom String)
     }
 
-    regel = validierungsregeln.get(bezeichner_index)  # Wert vom Dictionary überprüfen
-    if not regel:
-        return True
-
-    if not re.match(regel, daten):
-        print(f"Ungültige {bezeichner_daten[bezeichner_index]}.")
+    regel = validierungsregeln.get(bezeichner_index)  # Abrufen der Validierungsregel
+    if regel and not re.match(regel, daten):  # Wenn keine Regel vorhanden ist
+        print(f"Ungültige Eingabe: {bezeichner_daten[bezeichner_index]}")
         return False
     return True
 
 
-def filtern(gespeicherte_daten):
+# Funktion zur Eingabe der Daten
+def eingabe_daten(gespeicherte_daten=None):  # Zuerst None da bei jedem Aufruf Dictionary gelöscht wird
+    if gespeicherte_daten is None:  # Wenn kein Dictionary vorhanden ist (Erstmaliges Aufrufen)
+        gespeicherte_daten = {}  # Dictionary initialisieren
+    person_id = len(gespeicherte_daten) + 1  # ID bei 1 beginnen (normal bei 0)
+
+    # Schleife zur Dateneingabe
     while True:
-        auswahl_filtern = input("Wollen Sie die Liste filtern? (ja/nein):\n")
-        if auswahl_filtern == "j":
-            print("Liste filtern")
-        elif auswahl_filtern == "n":
-            ausgabe_daten(gespeicherte_daten)
+        # Dictionary für jede einzelne Person erstellen
+        personen_daten = {}
+        for bezeichner in bezeichner_daten:
+            while True:
+                eingabe = input(f"Bitte geben Sie {bezeichner} ein: ")  # Benutzereingabe erfassen
+                if eingabe.strip() == "":  # Eingabe darf nicht leer sein
+                    print("Feld darf nicht leer sein")
+                    continue
+                if not validierung(eingabe, bezeichner_daten.index(bezeichner)):  # Überprüfen der Eingaben
+                    continue
+                break
+            # Daten zum Dictionary hinzufügen
+            personen_daten[bezeichner] = eingabe
+
+        gespeicherte_daten[person_id] = personen_daten  # Speichert die Person mit der ID Nummer
+        person_id += 1  # Erhöhen der ID
+
+        if not weitere_personen():  # Abfrage für weitere Personeneingabe
             break
-        else:
-            print(UNGUELTIGE_EINGABE)
+
+    ausgabe_daten(gespeicherte_daten)
+    abfrage(gespeicherte_daten)
+    return gespeicherte_daten
 
 
+def weitere_personen():
+    abfrage_weitere_personen = input("Möchten Sie weitere Personen erfassen? (ja/nein):\n")
+    if abfrage_weitere_personen == "j":
+        return True
+    elif abfrage_weitere_personen == "n":
+        return False
+    else:
+        print(UNGUELTIGE_EINGABE)
+
+
+# Funktion zum Filtern der Daten
+def filtern(gespeicherte_daten):
+    gefilterte_liste = []
+
+    print("Verfügbare Bezeichner:")
+    # Schleife zur Filterung
+    for i, bezeichner in enumerate(bezeichner_daten, 1):
+        print(f"{i}. {bezeichner}")
+
+    bezeichner_nr = int(input("Bitte geben Sie die Nummer des Bezeichners ein, nach dem Sie filtern möchten: "))
+    bezeichner = bezeichner_daten[bezeichner_nr - 1]
+    filterkriterium = input(f"Bitte geben Sie das Filterkriterium für '{bezeichner}' ein: ")
+
+    for person in gespeicherte_daten.values():
+        if person[bezeichner] == filterkriterium:
+            gefilterte_liste.append(person)
+
+    if gefilterte_liste:  # Überprüfen ob die Liste nicht leer ist
+        print("\nGefilterte Daten:")
+        for person in gefilterte_liste:
+            for bezeichner, daten in person.items():
+                print(f"{bezeichner}: {daten}")
+            print()
+    else:
+        print("Keine Daten gefunden, die dem Filterkriterium " + filterkriterium + " entsprechen.")
+
+
+# Funktion zur Ausgabe der Daten
+def ausgabe_daten(gespeicherte_daten):
+    print("Gespeicherte Daten:\n")
+    for person_id, person in gespeicherte_daten.items():
+        print(f"Person {person_id}")
+        for key, value in person.items():
+            print(f"{key}: {value}")
+        print()
+
+
+# Funktion für weitere Aktionen im Programm
 def abfrage(gespeicherte_daten):
     while True:
-        print("Was möchten Sie tun?\n1. Benutzer ändern\n2. Liste filtern\n3. Liste ausgeben\n")
+        print("Was möchten Sie tun?\n1. Benutzer ändern\n2. Liste filtern\n3. Benutzer hinzufügen\n")
 
         auswahl = input("Bitte geben Sie die Nummer der gewünschten Aktion ein: ")
         if auswahl == "1":
             benutzer_aendern(gespeicherte_daten)
-            return True
         elif auswahl == "2":
-            print("Liste filtern")
-            return True
+            filtern(gespeicherte_daten)
         elif auswahl == "3":
-            print(gespeicherte_daten.to_string())  # DataFrame ausgeben
-            exit()
+            eingabe_daten(gespeicherte_daten)
         else:
             print(UNGUELTIGE_EINGABE)
 
 
+# Funktion für Benutzeränderung
 def benutzer_aendern(gespeicherte_daten):
-    # Liste der vorhandenen Benutzer anzeigen
-    print("Liste der vorhandenen Benutzer:\n" + gespeicherte_daten.to_string(index=True))
+    print("Liste der vorhandenen Benutzer:\n")
 
-    # ID des zu ändernden Benutzers abfragen
-    id_zum_aendern = int(input("Bitte geben Sie die ID des zu ändernden Benutzers ein: ")) - 1
-    if 0 <= id_zum_aendern < len(gespeicherte_daten):
-        print(f"Ändern der Daten für Benutzer mit ID {id_zum_aendern + 1}:")
-        gespeicherte_daten.at[id_zum_aendern] = eingabe_daten()
-        print("Benutzer erfolgreich geändert.")
-        abfrage(gespeicherte_daten)
+    for person_id, person in gespeicherte_daten.items():
+        print(f"ID: {person_id}")
+
+        for bezeichner, daten in person.items():
+            print(f"{bezeichner}: {daten}")
+        print()
+
+    # ID abfragen
+    id_zum_aendern = int(input("Bitte geben Sie die ID des zu ändernden Benutzers ein: "))
+    # Überprüfen, ob ID vorhanden ist
+    if id_zum_aendern in gespeicherte_daten:
+        print(f"Ändern der Daten für Benutzer mit ID {id_zum_aendern}:")
+
+        for bezeichner in bezeichner_daten:
+            aktuelle_daten = gespeicherte_daten[id_zum_aendern][bezeichner]  # Anzeigen der "alten" Werte
+            neue_daten = input(f"{bezeichner}: ({aktuelle_daten}) ")
+            # Überprüfen, ob neue Daten eingegeben wurden
+            if neue_daten.strip() == "":
+                print("Eingabe darf nicht leer sein.")
+            else:
+                # Validierung der neuen Daten
+                if validierung(neue_daten, bezeichner_daten.index(bezeichner)):
+                    gespeicherte_daten[id_zum_aendern][bezeichner] = neue_daten
+                else:
+                    # Fehlermeldung bei ungültiger Eingabe und Beibehalten der alten Daten
+                    print(f"Ungültige Eingabe für {bezeichner}. Behalten der alten Daten.")
+        ausgabe_daten(gespeicherte_daten)
     else:
-        print("Ungültige ID.")
+        print("Ungültige ID.")  # Fehlermeldung für ungültige Benutzer-ID
 
 
 # Hier beginnt der Main-Teil
-print("Herzlich Willkommen zur Datenerfassung.\nBitte geben Sie die Personendaten ein:\n")
-eingabe_daten()
+if __name__ == "__main__":
+    print("Herzlich Willkommen zur Datenerfassung.\nBitte geben Sie die Personendaten ein:\n")
+    eingabe_daten()
